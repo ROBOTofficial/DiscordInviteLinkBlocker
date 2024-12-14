ab011a3dc65ca04d2cc2d5a44d948557c24412f5
ROBOT <tkata12@outlook.jp>
2024-12-15 05:41:03 +0900

    招待リンク検知用のメゾット追加

---
+++ src/utils/regex.ts
@@ -0,0 +1,26 @@
+import { inviteLinkChecker } from "./url";
+
+const URL_REGEXP = /^https?:\/\/[\w/:%#\$&\?\(\)~\.=\+\-]+$/mi;
+
+const URL_REGEXP_NO_HTTP = /discord\.gg\/[A-Za-z0-9]+/mi;
+
+const URL_REGEXP_INVITE_NO_HTTP = /discord\.com\/invite(\/|\\)[A-Za-z0-9]+/mi;
+
+export async function findUrls(content: string) {
+    const regExp = /(https?:\/\/[\w!\?/\+\-_~=;\.,\*&@#$%\(\)'\[\]]+)|(discord\.gg\/[A-Za-z0-9]+)|(discord\.com\/invite(\/|\\)[A-Za-z0-9]+)/mi;
+    const lines = content.split("\n");
+
+    for (const line of lines) {
+        const urls = line.split(regExp);
+        for (const url of urls) {
+            if (URL_REGEXP_NO_HTTP.test(url) || URL_REGEXP_INVITE_NO_HTTP.test(url)) {
+                return true;
+            }
+            if (URL_REGEXP.test(url) && await inviteLinkChecker(url)) {
+                return true;
+            }
+        }
+    }
+
+    return false;
+}
