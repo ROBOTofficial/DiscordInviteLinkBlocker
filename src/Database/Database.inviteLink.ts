a9d41f42151e1c8336f74d33885d4ad811f284e0
ROBOT <tkata12@outlook.jp>
2024-12-15 18:42:30 +0900

    inviteLinkテーブルのwrapperを追加

---
+++ src/Database/Database.inviteLink.ts
@@ -0,0 +1,39 @@
+import { errorHandling } from "../utils/error";
+
+import type { Prisma } from "@prisma/client";
+import type { DefaultArgs } from "@prisma/client/runtime/library";
+
+export class InviteLink {
+    constructor(private readonly table: Prisma.InviteLinkDelegate<DefaultArgs>) {
+    }
+
+    public async update(url: string) {
+        try {
+            const element = await this.table.findFirst({ where: { url } });
+            const time = BigInt(Date.now());
+            if (element) {
+                await this.table.updateMany({
+                    where: { url: element.url },
+                    data: { time },
+                })
+            } else {
+                await this.table.create({
+                    data: { url, time }
+                })
+            }
+            return true;
+        } catch (error) {
+            errorHandling(error);
+            return false;
+        }
+    }
+
+    public async includeArchives(url: string) {
+        try {
+            return (await this.table.findMany()).map(value => value.url).includes(url);
+        } catch (error) {
+            errorHandling(error);
+            return false;
+        }
+    }
+}
--- src/Database/index.ts
+++ src/Database/index.ts
@@ -1,2 +1,8 @@
+import { PrismaClient } from "@prisma/client";
+import { InviteLink } from "./Database.inviteLink";
 
-export class Database {}
+export class Database {
+    private readonly prisma = new PrismaClient();
+
+    public readonly inviteLink = new InviteLink(this.prisma.inviteLink);
+}
