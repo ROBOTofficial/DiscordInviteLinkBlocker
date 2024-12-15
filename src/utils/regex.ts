f828053283721cf2ce39e3acdb3276244175749a
ROBOT <tkata12@outlook.jp>
2024-12-15 18:35:26 +0900

    regexの返り値変更

--- src/utils/regex.ts
+++ src/utils/regex.ts
@@ -6,7 +6,7 @@ const URL_REGEXP_NO_HTTP = /discord\.gg\/[A-Za-z0-9]+/mi;
 
 const URL_REGEXP_INVITE_NO_HTTP = /discord\.com\/invite(\/|\\)[A-Za-z0-9]+/mi;
 
-export async function findUrls(content: string) {
+export async function findUrls(content: string): Promise<string | null> {
     const regExp = /(https?:\/\/[\w!\?/\+\-_~=;\.,\*&@#$%\(\)'\[\]]+)|(discord\.gg\/[A-Za-z0-9]+)|(discord\.com\/invite(\/|\\)[A-Za-z0-9]+)/mi;
     const lines = content.split("\n");
 
@@ -14,13 +14,13 @@ export async function findUrls(content: string) {
         const urls = line.split(regExp);
         for (const url of urls) {
             if (URL_REGEXP_NO_HTTP.test(url) || URL_REGEXP_INVITE_NO_HTTP.test(url)) {
-                return true;
+                return url;
             }
             if (URL_REGEXP.test(url) && await inviteLinkChecker(url)) {
-                return true;
+                return url;
             }
         }
     }
 
-    return false;
+    return null;
 }
