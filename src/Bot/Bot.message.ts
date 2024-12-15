b5bf0536352def4976291b07791c2881fe789747
ROBOT <tkata12@outlook.jp>
2024-12-15 18:29:55 +0900

    url削除機能を実装

--- src/Bot/Bot.message.ts
+++ src/Bot/Bot.message.ts
@@ -1,4 +1,4 @@
-
+import { PermissionsBitField } from "discord.js";
 import { findUrls } from "../utils/regex";
 
 import type { Client, Message, OmitPartialGroupDMChannel } from "discord.js";
@@ -7,7 +7,7 @@ export class BotMessage {
     constructor(private readonly client: Client) {}
 
     public async create(message: OmitPartialGroupDMChannel<Message<boolean>>): Promise<void> {
-        if (await findUrls(message.content)) {
+        if (message.member && !message.member.permissions.has(PermissionsBitField.Flags.Administrator) && await findUrls(message.content)) {
             return void await message.delete();
         }
     }
