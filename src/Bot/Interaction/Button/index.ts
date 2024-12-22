7b85eb7d032f9da5a08d08c5fa5511e4731ba59f
ROBOT <tkata12@outlook.jp>
2024-12-22 23:14:43 +0900

    discordのbutton用のwrapper作成

---
+++ src/Bot/Interaction/Button/index.ts
@@ -0,0 +1,27 @@
+import { codeBlock } from "../../../utils/codeblock";
+
+import type { ButtonInteraction, CacheType, Client } from "discord.js";
+import type { ButtonsBase } from "./Buttons.base";
+
+export class Buttons {
+	public readonly buttons: ButtonsBase[]
+
+	constructor(private readonly client: Client) {
+		this.buttons = [
+		];
+	}
+	async reply(interaction: ButtonInteraction<CacheType>): Promise<void> {
+		for (const button of this.buttons) {
+			if (button.customId === interaction.customId) {
+				const result = await button.reply(interaction);
+				if (result) {
+					return void await interaction.reply({ content: codeBlock(`Error: ${result.content}`), ephemeral: true });
+				} else {
+					return result;
+				}
+			}
+		}
+
+		return void await interaction.reply({ content: "Command Not Found", ephemeral: true });
+	}
+}
--- src/Bot/Interaction/index.ts
+++ src/Bot/Interaction/index.ts
@@ -1,19 +1,25 @@
 import { Client } from "discord.js";
 
 import { Commands } from "./Commands/index";
+import { Buttons } from "./Button/index";
 
 import type { CacheType, Interaction } from "discord.js";
 
 export class InteractionResponse {
     public readonly commands: Commands;
+    public readonly buttons: Buttons;
 
     constructor(private readonly client: Client) {
         this.commands = new Commands(this.client);
+        this.buttons = new Buttons(this.client);
     }
 
     async reply(interaction: Interaction<CacheType>): Promise<void> {
         if (interaction.isContextMenuCommand()) {
             return;
+        }
+        if (interaction.isButton()) {
+            return await this.buttons.reply(interaction);
         }
         if (interaction.isCommand()) {
             return await this.commands.reply(interaction);
