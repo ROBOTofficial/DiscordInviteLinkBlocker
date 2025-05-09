80296760df9a9539cbd8e01760fd456a5dee29cb
ROBOT <tkata12@outlook.jp>
2025-05-09 19:32:04 +0900

    UserSettingsテーブルを追加

--- src/Database/index.ts
+++ src/Database/index.ts
@@ -1,10 +1,12 @@
 import { PrismaClient } from "@prisma/client";
 import { InviteLink } from "./Database.inviteLink";
 import { Settings } from "./Settings/index";
+import { User } from "./Database.user";
 
 export class Database {
     private readonly prisma = new PrismaClient();
 
     public readonly inviteLink = new InviteLink(this.prisma.inviteLink);
     public readonly settings = new Settings(this.prisma);
+    public readonly user = new User(this.prisma.userSetting);
 }
